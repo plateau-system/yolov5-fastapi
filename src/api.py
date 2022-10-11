1f34cc78e77b957cdf72ab2c91e2d02799989135
Nishiumi_Daina <71867595+nishiumidaina@users.noreply.github.com>
2022-10-12 04:56:00 +0900

    day,week,monthごとに情報を蓄積するスクリプトを追加。

--- src/api.py
+++ src/api.py
@@ -11,7 +11,7 @@ from fastapi import FastAPI
 import asyncio
 import mysql.connector
 
-#FastAPI(API)、count(初期動作のトリガー)、response_lis(初期値)
+# FastAPI(API)、count(初期動作のトリガー)、response_lis(初期値)
 app = FastAPI()
 count=0
 response_lis = [{
@@ -41,18 +41,17 @@ def save_frame_camera_cycle():
         # 接続状況確認
         print(conn.is_connected())
         cur = conn.cursor(buffered=True)
-        cur.execute("SELECT spots_name, spots_url FROM spots WHERE spots_status=0")
+        cur.execute("SELECT id, spots_name, spots_url, spots_day, spots_week, spots_month FROM spots WHERE spots_status=0")
         db_lis = cur.fetchall()
-        print(db_lis[0])
+        # DB接続終了
         cur.close()
-        #detect_lis(一時保存用)
-        detect_lis = []
 
+        detect_lis = []
         if os.path.isdir('yolov5-plateau-system/runs/detect/'):
             shutil.rmtree('yolov5-plateau-system/runs/detect/')
 
         for i in range(len(db_lis)):
-            video = pafy.new(db_lis[i][1])
+            video = pafy.new(db_lis[i][2])
             best = video.getbest(preftype="mp4")
             cap = cv2.VideoCapture(best.url)
             
@@ -67,7 +66,11 @@ def save_frame_camera_cycle():
             proc = subprocess.run(['python', 'yolov5-plateau-system/main.py','--save-txt'], stdout=PIPE, stderr=PIPE)
             proc_str = proc.stdout.decode('utf-8').split()
             proc_int = [int(s) for s in proc_str]
-            #検出が終わった画像の削除
+            print(proc_str)
+            # 返ってきた結果が空だった時の処理
+            if not proc_int:
+                proc_int = [0, 0, 0, 0]
+            # 検出が終わった画像の削除
             target_dir = 'yolov5-plateau-system/data/images/'
             shutil.rmtree(target_dir)
             os.mkdir(target_dir)
@@ -76,7 +79,7 @@ def save_frame_camera_cycle():
         response_lis.clear()
         for i2 in range(len(detect_lis)):
             data = {
-                'name' : db_lis[i2][0],
+                'name' : db_lis[i2][1],
                 'person' : detect_lis[i2][0],
                 'bicycle' : detect_lis[i2][1],
                 'car' : detect_lis[i2][2],
@@ -84,15 +87,102 @@ def save_frame_camera_cycle():
             }
             response_lis.append(data)
 
-        print(detect_lis)
-        #30秒に1回更新（検出時間があるため正確ではない）
-        sleep(30)
+        # day 1～24時間、最後にリセット
+        # week 過去７日間
+        # month 過去３０日間
+        for i3 in range(len(db_lis)):
+            # day
+            if db_lis[i3][3] == 'None':
+                count_day = '%s,%s,%s,%s,' % (detect_lis[i3][0], detect_lis[i3][1], detect_lis[i3][2], detect_lis[i3][3])
+                cur = conn.cursor(buffered=True)
+                sql = ("UPDATE spots SET spots_day = %s WHERE id = %s")
+                param = (count_day, db_lis[i3][0])
+                cur.execute(sql, param)
+                conn.commit()
+                cur.close()
+            else:
+                all_day = db_lis[i3][3].split(',')
+                all_day.remove('')
+                if len(all_day) >= 96:
+                    count_day = '%s,%s,%s,%s,' % (detect_lis[i3][0], detect_lis[i3][1], detect_lis[i3][2], detect_lis[i3][3])
+                    cur = conn.cursor(buffered=True)
+                    sql = ("UPDATE spots SET spots_day = %s WHERE id = %s")
+                    param = (count_day, db_lis[i3][0])
+                    cur.execute(sql, param)
+                    conn.commit()
+                    cur.close()         
+
+                    # week
+                    all_day_int = [int(i4) for i4 in all_day]
+                    all_day_re = convert_1d_to_2d(all_day_int, 4)
+                    ave_day_person = sum([i5[0] for i5 in all_day_re]) / 24
+                    ave_day_bicycle = sum([i6[1] for i6 in all_day_re]) / 24
+                    ave_day_car = sum([i7[2] for i7 in all_day_re]) / 24
+                    ave_day_motorcycle = sum([i8[3] for i8 in all_day_re]) / 24
+
+                    # 初回のみ初期値として0を６日間分入れる+今日の結果
+                    if db_lis[i3][4] == 'None':
+                        none_week = '0,0,0,0,' * 6
+                        count_week = '%s%s,%s,%s,%s,' % (none_week, ave_day_person, ave_day_bicycle, ave_day_car, ave_day_motorcycle)
+                        cur = conn.cursor(buffered=True)
+                        sql = ("UPDATE spots SET spots_week = %s WHERE id = %s")
+                        param = (count_week, db_lis[i3][0])
+                        cur.execute(sql, param)
+                        conn.commit()
+                        cur.close()
+                    else:
+                        all_week = db_lis[i3][4].split(',')
+                        del all_week[0:5]
+                        count_week = '%s%s,%s,%s,%s,' % (all_week,ave_day_person, ave_day_bicycle, ave_day_car, ave_day_motorcycle)
+                        cur = conn.cursor(buffered=True)
+                        sql = ("UPDATE spots SET spots_week = %s WHERE id = %s")
+                        param = (count_week, db_lis[i3][0])
+                        cur.execute(sql, param)
+                        conn.commit()
+                        cur.close()
+
+                    # month
+                    # 初回のみ初期値として0を29日間分入れる+今日の結果
+                    if db_lis[i3][5] == 'None':
+                        none_month = '0,0,0,0,' * 29
+                        count_month = '%s%s,%s,%s,%s,' % (none_month, ave_day_person, ave_day_bicycle, ave_day_car, ave_day_motorcycle)
+                        cur = conn.cursor(buffered=True)
+                        sql = ("UPDATE spots SET spots_month = %s WHERE id = %s")
+                        param = (count_month, db_lis[i3][0])
+                        cur.execute(sql, param)
+                        conn.commit()
+                        cur.close()
+                    else:
+                        all_month = db_lis[i3][5].split(',')
+                        del all_month[0:5]
+                        count_month = '%s%s,%s,%s,%s,' % (all_month,ave_day_person, ave_day_bicycle, ave_day_car, ave_day_motorcycle)
+                        cur = conn.cursor(buffered=True)
+                        sql = ("UPDATE spots SET spots_week = %s WHERE id = %s")
+                        param = (count_month, db_lis[i3][0])
+                        cur.execute(sql, param)
+                        conn.commit()
+                        cur.close()
+
+                else:
+                    count_day = '%s%s,%s,%s,%s,' % (db_lis[i3][3], detect_lis[i3][0], detect_lis[i3][1], detect_lis[i3][2], detect_lis[i3][3])
+                    cur = conn.cursor(buffered=True)
+                    sql = ("UPDATE spots SET spots_day = %s WHERE id = %s")
+                    param2 = (count_day,db_lis[i3][0])
+                    cur.execute(sql,param2)
+                    conn.commit()
+                    cur.close()
+
+        # １時間に1回更新（検出時間があるため正確ではない）
+        sleep(3600)
+# リストの変換用
+def convert_1d_to_2d(l, cols):
+    return [l[i:i + cols] for i in range(0, len(l), cols)]
 
 @app.get("/detect")
 def proc1():
     print(count)
     global response_lis
-    #もしcountが0の時関数を起動（最初の一回のみ）
+    # もしcountが0の時関数を起動（最初の一回のみ）
     if count == 0:
         asyncio.new_event_loop().run_in_executor(None, save_frame_camera_cycle)
     return response_lis
